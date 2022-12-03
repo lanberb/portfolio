31cc82490e5bb3e8fe86ce32bbd23e660e86e23c
lanberb <lanberb.2@gmail.com>
2022-12-04 00:11:12 +0900

    empty

---
+++ src/styles/Global.tsx
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  @media (prefers-color-scheme: dark) {
+    :root {
+    }
+  }
+`;
