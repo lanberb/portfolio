1d9143bed83327e0754fc78d9d33f1f305f4090e
lanberb <lanberb.2@gmail.com>
2025-02-26 23:59:10 +0900

    feat: styledを剥がしてemotion導入とappレイヤー追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,23 @@
+import { CanvasContext } from "@/components/hooks/useCanvas";
+import { ThemeStateProvider } from "@/components/hooks/useTheme";
+import { Box } from "@/components/unit/Box";
+import { Canvas } from "@/components/unit/Canvas";
 import type { AppProps } from "next/app";
+import { useRef } from "react";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  return (
+    <ThemeStateProvider>
+      <CanvasContext.Provider value={canvasRef}>
+        <Canvas ref={canvasRef} position="absolute" zIndex={9999} />
+        <Box position="relative" zIndex={0}>
+          <Component {...pageProps} />
+        </Box>
+      </CanvasContext.Provider>
+    </ThemeStateProvider>
+  );
 };
 
 export default App;
