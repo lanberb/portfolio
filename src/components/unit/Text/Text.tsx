bf255188405a4a93b1f9293df762a53279ede876
Nao <lanberb.2@gmail.com>
2025-10-01 23:39:48 +0900

    Revert "feat: Add Cloudflare Pages deployment and configuration (#2)"
    This reverts commit 51ec797d6f9090daa1c35bb39b8e5c15bb5487e4.

--- src/components/unit/Text/Text.tsx
+++ src/components/unit/Text/Text.tsx
@@ -1,9 +1,9 @@
 import { type BaseProps, type MarginProps, type PaddingProps, type TypographyProps, base, margins, paddings, typography } from "@/styles/mixins";
 import styled from "@emotion/styled";
-import type { ElementType, LabelHTMLAttributes } from "react";
+import type { LabelHTMLAttributes } from "react";
 
 interface Props extends TypographyProps, BaseProps, MarginProps, PaddingProps {
-  as?: ElementType;
+  as?: keyof JSX.IntrinsicElements;
   htmlFor?: LabelHTMLAttributes<HTMLLabelElement>["htmlFor"];
 }
 
