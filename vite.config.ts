2d1f2904c2fd14176e1b53d9b2ed39235d35d11b
lanberb <lanberb.2@gmail.com>
2025-11-08 21:59:53 +0900

    feat: deploy to workers

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import react from "@vitejs/plugin-react";
+import { cloudflare } from '@cloudflare/vite-plugin';
+import react from '@vitejs/plugin-react';
 import path from "path";
-import { defineConfig } from "vite";
-import { viteInjectSvgPlugin } from "./plugins/viteInjectSvgPlugin";
+import { type PluginOption, defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), viteInjectSvgPlugin()],
+  plugins: [react(), cloudflare() as PluginOption],
   build: {
     rollupOptions: {
       input: path.resolve(__dirname, "src/client/main.tsx"),
